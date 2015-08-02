7e98a4988e88fb6fae6cede3d47cc6a28c3a3a4d
Mohammed Yaseen <connect2yaseen@gmail.com>
2015-08-02 17:52:41 +0530

    Initial Checkin
    Step towards my automation framework

---
+++ AutomationFramework/Framework.Core/Framework.Core/Basic_WebElements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+
+namespace Framework.Core
+{
+    public class Basic_WebElements
+    {
+        /* <Summary>
+         This method will lauch browser and go to particular url
+         <param name="url"> type the url <param>
+         <summary>  */
+
+        public void gotoUrl(string url)
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(url);
+
+        }
+    }
+}
---
+++ AutomationFramework/Framework.Core/Testlibararies/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework.Core;
+using NUnit.Framework;
+
+namespace Testlibararies
+{
+    [TestFixture]
+    class Program
+    {
+
+        [Test]
+        public void test1()
+        {
+            var web_elements = new Basic_WebElements();
+
+            web_elements.gotoUrl("http://www.google.com");
+            
+        }
+    }
+}

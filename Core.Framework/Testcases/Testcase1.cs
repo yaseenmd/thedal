bd558efba6de850376ab925e914335642ac95ea4
Mohammed Yaseen <connect2yaseen@gmail.com>
2015-08-29 14:48:03 +0530

    writing a first test to verify build is successful and test is running in Nunit

---
+++ Core.Framework/Testcases/Testcase1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Core.Framework;
+
+namespace Testcases
+{
+    [TestFixture]
+    public class Testcase1
+    {
+        string url = "http:demo.testlink.org";
+    [Test]
+
+        public void tc_testcase1()
+        {
+            var basic_html = new Basic_html_elements();
+            basic_html.openurl(url);
+            
+        }
+
+
+    }
+}

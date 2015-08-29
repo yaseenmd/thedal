dbfa37e94d6cda04daac6366485709c0bf2e9fe6
Mohammed Yaseen <connect2yaseen@gmail.com>
2015-08-29 13:33:19 +0530

    created new method openurl()

--- Core.Framework/Core.Framework/Basic_html_elements.cs
+++ Core.Framework/Core.Framework/Basic_html_elements.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace Core.Framework
 {
+  
     public class Basic_html_elements
     {
+        
+      /* this is used to navigate to the particular url
+       * url is param.
+       * type the url where you want to go*/
+
+        public void openurl(string url)
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(url);
+        }
+
+
     }
 }
